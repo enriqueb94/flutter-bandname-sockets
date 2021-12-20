c27ff6ce0ce1939e846bc2cca4ba4fa37291eaa5
Enrique Barreto <enriquebdfx@gmail.com>
2021-12-20 18:24:59 -0400

    cambios en pie chart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -165,38 +165,6 @@ class _HomePageState extends State<HomePage> {
       Colors.yellow[200] as Color
     ];
 
-    return SizedBox(
-      width: double.infinity,
-      height: 200,
-      child: PieChart(
-        dataMap: dataMap,
-        animationDuration: const Duration(milliseconds: 800),
-        chartLegendSpacing: 32,
-        chartRadius: MediaQuery.of(context).size.width / 3.2,
-        colorList: colorList,
-        initialAngleInDegree: 0,
-        chartType: ChartType.ring,
-        ringStrokeWidth: 22,
-        centerText: "Bands",
-        legendOptions: const LegendOptions(
-          showLegendsInRow: false,
-          legendPosition: LegendPosition.right,
-          showLegends: true,
-          legendShape: BoxShape.circle,
-          legendTextStyle: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        chartValuesOptions: const ChartValuesOptions(
-          showChartValueBackground: true,
-          showChartValues: true,
-          showChartValuesInPercentage: false,
-          showChartValuesOutside: false,
-          decimalPlaces: 1,
-        ),
-        // gradientList: ---To add gradient colors---
-        // emptyColorGradient: ---Empty Color gradient---
-      ),
-    );
+    return PieChart(dataMap: dataMap);
   }
 }
